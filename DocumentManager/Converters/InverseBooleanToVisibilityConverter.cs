d2bdbbf18ce66dacedb2c8bc2305fe76c7602d5f
zzfima <zzfima@gmail.com>
2026-03-06 15:40:41 +0200

    add Humanizer for sizes

--- DocumentManager/Converters/InverseBooleanToVisibilityConverter.cs
+++ DocumentManager/Converters/InverseBooleanToVisibilityConverter.cs
@@ -4,33 +4,17 @@ using System.Windows.Data;
 
 namespace DocumentManager.Converters
 {
-	public class InverseBooleanToVisibilityConverter : IValueConverter
-	{
-		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
-		{
-			if (value is bool boolValue)
-			{
-				return boolValue ? Visibility.Collapsed : Visibility.Visible;
-			}
-			return Visibility.Visible;
-		}
+    public class InverseBooleanToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool boolValue)
+            {
+                return boolValue ? Visibility.Collapsed : Visibility.Visible;
+            }
+            return Visibility.Visible;
+        }
 
-		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
-		{
-			throw new NotImplementedException();
-		}
-	}
-
-	public class StringToVisibilityConverter : IValueConverter
-	{
-		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
-		{
-			return string.IsNullOrEmpty(value as string) ? Visibility.Collapsed : Visibility.Visible;
-		}
-
-		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
-		{
-			throw new NotImplementedException();
-		}
-	}
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => throw new NotImplementedException();
+    }
 }
---
+++ DocumentManager/Converters/StringToVisibilityConverter.cs
@@ -0,0 +1,13 @@
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace DocumentManager.Converters
+{
+    public class StringToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) => string.IsNullOrEmpty(value as string) ? Visibility.Collapsed : Visibility.Visible;
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => throw new NotImplementedException();
+    }
+}
